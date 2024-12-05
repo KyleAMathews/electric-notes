b4c097fabfd2637680945531cc97825b405253ab
Kyle Mathews <mathews.kyle@gmail.com>
2024-12-05 15:02:33 -0700

    Improve homepage placeholder

--- src/components/Editor/EditorPlaceholder.tsx
+++ src/components/Editor/EditorPlaceholder.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function EditorPlaceholder() {
   return (
-    <div className="flex-1 flex items-center justify-center text-gray-500">
+    <div className="flex-1 flex items-center justify-center text-gray-500 mt-16">
       Select or create a note to start editing
     </div>
   );
